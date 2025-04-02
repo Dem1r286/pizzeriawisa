0681b0c2244d996f75c97e96b1c98db0eacb1b6e
Dem1r286 <yourgithubemail@gmail.com>
2025-04-02 15:53:45 +0200

    dasd

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -19,8 +19,8 @@ const Footer = () => {
         </div>
 
         {/* Right: Google Maps */}
-        <div className="mt-6 w-1/3">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4382.738588335883!2d18.401471504572623!3d43.858428572748146!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4758c91fc72a27b5%3A0xbd2c27a37c39e043!2sPizzeria%20Wisa!5e1!3m2!1sen!2sba!4v1743601768273!5m2!1sen!2sba" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <div className="mt-6">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4382.738588335883!2d18.401471504572623!3d43.858428572748146!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4758c91fc72a27b5%3A0xbd2c27a37c39e043!2sPizzeria%20Wisa!5e1!3m2!1sen!2sba!4v1743601768273!5m2!1sen!2sba" width="350" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
       </div>
 
