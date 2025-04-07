d755357206cc1c65c616d6c60dccfd3f39e27d3b
Dem1r286 <yourgithubemail@gmail.com>
2025-04-07 08:48:18 +0200

    Updates

--- assets/Menu-DnjsZ5HC.js
+++
@@ -1,6 +0,0 @@
-import{j as e,L as t}from"./index-DwdOLKfT.js";import{c as a}from"./createLucideIcon-w28QDIOU.js";/**
- * @license lucide-react v0.487.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const c=[["path",{d:"M18 6 6 18",key:"1bl5f8"}],["path",{d:"m6 6 12 12",key:"d8bk6v"}]],l=a("x",c),r=({handleMenuToggle:s})=>e.jsxs("div",{id:"menu",className:"absolute top-0 left-0 w-screen h-screen bg-[#222222] z-100",children:[e.jsx(t,{to:"/",className:"absolute top-10 right-10 text-3xl text-white z-100",onClick:s,children:e.jsx(l,{className:"w-10 h-10"})}),e.jsx("div",{className:"flex justify-center items-center h-full text-white",children:e.jsx("div",{className:"relative px-30 max-w-[1700px]",children:e.jsx("img",{src:"/assets/menu.webp",alt:"Menu",className:"rounded-2xl"})})})]});export{r as default};
