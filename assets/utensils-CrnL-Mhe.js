afab5de524d5314af1af44e25f7825ee4240fa6d
Dem1r286 <yourgithubemail@gmail.com>
2025-04-08 10:52:17 +0200

    Updates

--- assets/utensils-CrnL-Mhe.js
+++ assets/utensils-CrnL-Mhe.js
@@ -1,4 +1,4 @@
-import{c as e}from"./createLucideIcon-MWP3OAgq.js";/**
+import{c as e}from"./createLucideIcon-Ff5OCaz9.js";/**
  * @license lucide-react v0.487.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/x-DDwK8D3M.js
+++ assets/x-DDwK8D3M.js
@@ -1,4 +1,4 @@
-import{c as o}from"./createLucideIcon-MWP3OAgq.js";/**
+import{c as o}from"./createLucideIcon-Ff5OCaz9.js";/**
  * @license lucide-react v0.487.0 - ISC
  *
  * This source code is licensed under the ISC license.
