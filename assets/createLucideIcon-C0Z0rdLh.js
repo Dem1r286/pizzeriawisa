9b2ebe659e687098a1fbd7d2dadccc86d49c0bc3
Dem1r286 <yourgithubemail@gmail.com>
2025-04-08 10:28:28 +0200

    Updates

--- assets/createLucideIcon-C0Z0rdLh.js
+++ assets/createLucideIcon-C0Z0rdLh.js
@@ -1,4 +1,4 @@
-import{r as s}from"./index-KXM8s259.js";/**
+import{r as s}from"./index-DM-m4QjR.js";/**
  * @license lucide-react v0.487.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/x-Bgt-fwyQ.js
+++ assets/x-Bgt-fwyQ.js
@@ -1,4 +1,4 @@
-import{c as o}from"./createLucideIcon-kxuZSK10.js";/**
+import{c as o}from"./createLucideIcon-C0Z0rdLh.js";/**
  * @license lucide-react v0.487.0 - ISC
  *
  * This source code is licensed under the ISC license.
